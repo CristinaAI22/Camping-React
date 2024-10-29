6ba99ea04b6eca43c06789779ccbece3fdc6d5f6
MariucaO <onica.mariuca@gmail.com>
2024-10-29 16:10:41 +0200

    Fix Header component rendering and styles

---
+++ src/components/Header/Header.js
@@ -0,0 +1,22 @@
+import React from "react";
+import './Header.css';
+
+const Header = () => {
+    return (
+    <header className="header">
+        <h1 className="header-title">Bine ai venit la Camping "Gradina cu flori"</h1>
+        <nav className="header-nav">
+            <div className="logo"></div>
+            <ul className="navbar">
+                <li>Home</li>
+                <li>About</li>
+                <li>Services</li>
+                <li>Contact</li>
+            </ul>
+        </nav>
+
+    </header>
+    )
+}
+
+export default Header;
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Navbar = () => {
+    return (
+        <nav className="navbar">
+            <ul className="navbar-list"></ul>
+        </nav>
+    )
+};
+
+
+export default Navbar;
