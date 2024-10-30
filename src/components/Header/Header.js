a109a066f9dbc41395b7a9dfe87fab39794ebd6c
MariucaO <onica.mariuca@gmail.com>
2024-10-30 21:21:45 +0200

    Add style to Header and Navbar and a logo sample

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,23 +1,23 @@
 import React from "react";
-import styles from './Header.module.css';
+import styles from "./Header.module.css";
 import Navbar from "../Navbar/Navbar";
+import "../../App.css";
 
 const Header = () => {
-    return (
-    <header className="header">
-        <h1 className="header-title">Bine ai venit la Camping "Gradina cu flori"</h1>
-        <nav className="header-nav">
-            <div className="logo"></div>
-            <ul className="navbar">
-                <li>Home</li>
-                <li>About</li>
-                <li>Services</li>
-                <li>Contact</li>
-            </ul>
-        </nav>
+  return (
+    <header className={styles.header}>
+      <img 
+        src="/logo.png" 
+        alt="Camping Logo" 
+        className={styles.logo} 
+      />
+      <h1 className={styles.headerTitle}>
+        Bine ai venit la Camping "Gradina cu flori"
+      </h1>
 
+      <Navbar />
     </header>
-    )
-}
+  );
+};
 
 export default Header;
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,10 +1,23 @@
+// src/components/Navbar/Navbar.js
 import React from "react";
-import styles from "./Navbar.module.css";
-
+import styles from "./Navbar.module.css"; // Import the CSS module
 const Navbar = () => {
   return (
-    <nav className="navbar">
-      <ul className="navbar-list"></ul>
+    <nav className={styles.navbar}>
+      <ul className={styles.navLinks}> 
+        <li className={styles.navLink}> 
+          <a href="#home">Home</a>
+        </li>
+        <li className={styles.navLink}> 
+          <a href="#activities">Activities</a>
+        </li>
+        <li className={styles.navLink}> 
+          <a href="#gallery">Gallery</a>
+        </li>
+        <li className={styles.navLink}> 
+          <a href="#contact">Contact</a>
+        </li>
+      </ul>
     </nav>
   );
 };
