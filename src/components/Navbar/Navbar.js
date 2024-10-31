4a9ad013cb4134fc5c64bb2e15c94a001797ae69
MariucaO <onica.mariuca@gmail.com>
2024-10-31 14:36:59 +0200

    Hamburger menu with useState and use Ref

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,22 +1,40 @@
-// src/components/Navbar/Navbar.js
-import React from "react";
-import styles from "./Navbar.module.css"; // Import the CSS module
+import React, { useState, useRef, useEffect, useCallback } from "react";
+import { IconButton } from "@mui/material";
+import { Menu, Close } from "@mui/icons-material";
+import styles from "./Navbar.module.css";
+
 const Navbar = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const menuRef = useRef(null);
+
+  const toggleMenu = () => {
+    setIsMenuOpen((prevState) => !prevState);
+  };
+
+  const handleClickOutside = useCallback((event) => {
+    if (isMenuOpen && menuRef.current && !menuRef.current.contains(event.target)) {
+      setIsMenuOpen(false);
+    }
+  }, [isMenuOpen]); // Add isMenuOpen as a dependency
+
+  useEffect(() => {
+    document.addEventListener("mousedown", handleClickOutside);
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [handleClickOutside]); // Add handleClickOutside to the dependency array
+
   return (
     <nav className={styles.navbar}>
-      <ul className={styles.navLinks}> 
-        <li className={styles.navLink}> 
-          <a href="#home">Home</a>
-        </li>
-        <li className={styles.navLink}> 
-          <a href="#activities">Activities</a>
-        </li>
-        <li className={styles.navLink}> 
-          <a href="#gallery">Gallery</a>
-        </li>
-        <li className={styles.navLink}> 
-          <a href="#contact">Contact</a>
-        </li>
+      <IconButton onClick={toggleMenu} aria-label="Toggle menu">
+        {isMenuOpen ? <Close /> : <Menu />}
+      </IconButton>
+      <ul ref={menuRef} className={`${styles.navLinks} ${isMenuOpen ? styles.active : ""}`}>
+        <li className={styles.navLink}>Home</li>
+        <li className={styles.navLink}>About</li>
+        <li className={styles.navLink}>Services</li>
+        <li className={styles.navLink}>Contact</li>
       </ul>
     </nav>
   );
