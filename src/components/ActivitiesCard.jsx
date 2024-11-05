cf385c64593b135746a7a48ac85cf222de828193
MariucaO <onica.mariuca@gmail.com>
2024-11-05 22:25:10 +0200

    A card draft

---
+++ src/components/ActivitiesCard.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function ActivitiesCard() {
+    <div className='card'>
+        <img></img>
+        <h2>Plimbari cu bicicletele</h2>
+        <p>Oferim spre inchiriere biciclete electrice</p>
+    </div>
+}
+
+export default ActivitiesCard;
