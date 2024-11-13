95e0664389854a8e17df174d35589fb6026e39e6
CristinaAI22 <cristina.ionica22@gmail.com>
2024-11-13 12:44:53 +0200

    logo svg

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Map from "../components/Map/Map";
+import Weather from "../components/Weather/Weather";
 
 const Contact = () => {
   return (
     <>
       <Map />
+      <Weather />
     </>
   );
 };
